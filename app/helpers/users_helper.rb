e8b5ad5f368c6a151d0bfa496c1f44c1673a3db5
Raquel Bujans <look.away@gmail.com>
2015-05-08 10:59:08 -0400

    adding in fontawesome for iconfonts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,12 @@ module UsersHelper
 	def avatar_for(user)
 		if user.avatar_key?
 			image_tag(user.avatar_url, alt: user.name + ' avatar', class: "gravatar")
+		else
+			#return "<span class=\"fa-stack fa-fw user-avatar pull-right\">
+			#  <i class=\"fa fa-square-o fa-stack-2x\"></i>
+			#  <i class=\"fa fa-user fa-stack-1x\"></i>
+			#</span>".html_safe
+			return "<i class=\"fa fa-user fa-fw fa-border pull-right user-avatar\"></i>".html_safe
 		end
 	end
 
@@ -13,6 +19,7 @@ module UsersHelper
 			image_tag(user.avatar_thumbnail_url, alt: user.name + ' avatar thumbnail', class: "gravatar")
 		else 
 			# TODO: render default 'no profile pic chosen'
+			'<span class="icon-avatar-thumbnail"></span>'
 		end
 	end
 
