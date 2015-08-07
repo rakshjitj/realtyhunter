23cc9d0034ac5f5953588b3961a3d78b302afa0e
Raquel Bujans <look.away@gmail.com>
2015-08-07 11:18:43 -0400

    fixing up migration

--- db/migrate/20150807010936_add_more_indices.rb
+++ db/migrate/20150807010936_add_more_indices.rb
@@ -1,7 +1,7 @@
 class AddMoreIndices < ActiveRecord::Migration
   def change
 
-		#remove_column :units, :listing_agent
+		remove_column :units, :listing_agent
 		remove_column :landlords, :listing_agent_percentage, :integer
   	add_index :users, :auth_token
   end
