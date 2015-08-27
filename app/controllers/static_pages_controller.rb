ebcb819b302ed7d6fecfa2a8056ac9d21b9cdcbd
Raquel Bujans <look.away@gmail.com>
2015-08-26 20:48:26 -0400

    fix home page redirection

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,17 @@ class StaticPagesController < ApplicationController
 	skip_before_action :logged_in_user, only: [:home, :help]
 	
   def home
+  	if current_user
+	  	if current_user.is_external_vendor?
+	      redirect_to current_user
+	    elsif current_user.handles_residential?
+	      redirect_to residential_listings_path
+	    elsif current_user.handles_commercial?
+	      redirect_to commercial_listings_path
+	    else
+	      redirect_to current_user
+	    end
+	  end
   end
 
   def help
