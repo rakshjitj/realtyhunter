578ce5d1266df99892d712bbbd530ca9b4babcd8
Raquel Bujans <look.away@gmail.com>
2015-09-04 22:55:22 -0400

    update to update_columns

--- db/migrate/20150904231406_add_public_ur_ls.rb
+++ db/migrate/20150904231406_add_public_ur_ls.rb
@@ -8,13 +8,13 @@ class AddPublicUrLs < ActiveRecord::Migration
 
 		units = Unit.unarchived.where("buildings.company_id = ?", company.id).joins(building: :company)
 		units.each do |unit|
-			unit.update(public_url: "http://myspacenyc.com/listing/MYSPACENYC-#{unit.listing_id}")
+			unit.update_columns(public_url: "http://myspacenyc.com/listing/MYSPACENYC-#{unit.listing_id}")
 		end
 
 		users = User.unarchived.where(company_id: company.id).joins(:company)
 		users.each do |user|
 	    email_md5 = Digest::MD5.hexdigest(user.email)
-			user.update(public_url: "http://myspacenyc.com/agent/AGENT-#{email_md5}")
+			user.update_columns(public_url: "http://myspacenyc.com/agent/AGENT-#{email_md5}")
 		end
 
   end
