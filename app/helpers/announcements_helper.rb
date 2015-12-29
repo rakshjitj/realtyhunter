c9e8e9c9048cdae8fa4c4c1841816289ca27d2c6
Raquel Bujans <look.away@gmail.com>
2015-12-28 21:35:30 -0500

    fixing up announcements display so that deletion is only possible from main index

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,8 +1,8 @@
 module AnnouncementsHelper
 
 	def trim_announcement text
-		if !text.nil? && text.size > 75
-			text[0..75] + '...'
+		if !text.nil? && text.size > 70
+			text[0..70] + '...'
 		else
 			text
 		end
