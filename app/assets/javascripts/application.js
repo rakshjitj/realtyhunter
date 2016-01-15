001a2f439e2c48394026ed7a524884ad837fe06d
Raquel Bujans <raquel.bujans@gmail.com>
2016-01-15 08:00:56 -0500

    modals are full screen on mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,6 +45,25 @@ $(document).ready(function() {
 	      $(this).closest('form').submit();
 	    }
 	});
+
+	// .modal-backdrop classes
+	$(".modal-transparent").on('show.bs.modal', function () {
+	  setTimeout( function() {
+	    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+	  }, 0);
+	});
+	$(".modal-transparent").on('hidden.bs.modal', function () {
+	  $(".modal-backdrop").addClass("modal-backdrop-transparent");
+	});
+
+	$(".modal-fullscreen").on('show.bs.modal', function () {
+	  setTimeout( function() {
+	    $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+	  }, 0);
+	});
+	$(".modal-fullscreen").on('hidden.bs.modal', function () {
+	  $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
+	});
 });
 
 $(window).unload(function() {
