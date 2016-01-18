12c07387537ba9475cd7e763ae6d968614a5ca4b
Raquel Bujans <raquel.bujans@gmail.com>
2016-01-17 21:38:33 -0500

    attempting to update residential CSV generation to respond to filters. may still have bugs.

--- app/models/generate_residential_csv.rb
+++ app/models/generate_residential_csv.rb
@@ -2,7 +2,7 @@ class GenerateResidentialCSV
 	@queue = :generate_csv
 
 	# takes in residential listings
-	def self.perform(user_id)
+	def self.perform(user_id, params)
     UnitMailer.send_residential_csv(user_id).deliver_now
   end
 
