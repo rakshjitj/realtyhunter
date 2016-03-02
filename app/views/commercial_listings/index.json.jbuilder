bb146a8badd224668718bd7f7196fb4db3a91372
Raquel Bujans <raquel.bujans@gmail.com>
2016-03-01 21:56:44 -0500

    commercial, landlords work with infinite scrolling

--- app/views/commercial_listings/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@commercial_units) do |commercial_unit|
-  json.extract! commercial_unit, :id
-  json.url commercial_listing_url(commercial_unit, format: :json)
-end
--- app/views/residential_listings/index.js
+++ app/views/residential_listings/index.js
@@ -1,11 +1,11 @@
 // Append new data
 $("<%=j render partial: 'residential_listings/residential_listing', collection: @residential_units, locals: {res_images: @res_images} %>")
-  .appendTo($(".infinite-table"));
+  .appendTo($(".infinite-table-res"));
 Listings.hideSpinner();
 
 // Update pagination link
 <% if @residential_units.last_page? %>
-  $('.pagination').html("That's all, folks!");
+  $('.pagination').remove();
 <% else %>
   $('.pagination')
     .html("<%=j link_to_next_page(@residential_units, 'Load More', remote: true) %>");
