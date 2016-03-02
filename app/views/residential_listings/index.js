0b615797f5f94add7604619b26f74c2ea6c4f504
Raquel Bujans <raquel.bujans@gmail.com>
2016-03-01 23:24:57 -0500

    inifinite scroll working on res/mobile

--- app/views/residential_listings/index.js
+++ app/views/residential_listings/index.js
@@ -1,7 +1,11 @@
-
 // Append new data
-$("<%=j render partial: 'residential_listings/residential_listing', collection: @residential_units, locals: {res_images: @res_images} %>")
-  .appendTo($(".infinite-table-res"));
+if ($('.residential-desktop').length) {
+  $("<%=j render partial: 'residential_listings/residential_listing', collection: @residential_units, locals: {res_images: @res_images} %>")
+    .appendTo($(".infinite-table-res"));
+} else {
+  $("<%=j render partial: 'residential_listings/residential_listing_mobile_list_view', collection: @residential_units, as: :residential_listing, locals: {res_images: @res_images} %>")
+    .appendTo($(".infinite-table"));
+}
 Listings.hideSpinner();
 
 // Update pagination link
