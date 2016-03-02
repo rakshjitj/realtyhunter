210e54fd0486fcc380bab0baaa4a5145b341be0e
Raquel Bujans <raquel.bujans@gmail.com>
2016-03-01 21:19:58 -0500

    cleaning up pagination for residential

--- app/views/residential_listings/index.js
+++ app/views/residential_listings/index.js
@@ -1,11 +1,12 @@
 // Append new data
 $("<%=j render partial: 'residential_listings/residential_listing', collection: @residential_units, locals: {res_images: @res_images} %>")
   .appendTo($(".infinite-table"));
+Listings.hideSpinner();
 
 // Update pagination link
 <% if @residential_units.last_page? %>
   $('.pagination').html("That's all, folks!");
 <% else %>
   $('.pagination')
-    .html("<%=j link_to_next_page(@residential_units, 'Next Page', remote: true) %>");
+    .html("<%=j link_to_next_page(@residential_units, 'Load More', remote: true) %>");
 <% end %>
