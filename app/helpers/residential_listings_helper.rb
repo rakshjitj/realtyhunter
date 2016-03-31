85017c4f4ea1d64c0839b15ce7588e0f0f15b2dc
Raquel Bujans <raquel.bujans@gmail.com>
2016-03-31 14:41:52 -0400

    clearing up exceptions

---
+++ app/helpers/residential_listings_helper.rb
@@ -0,0 +1,50 @@
+module ResidentialListingsHelper
+
+	def small_header(residential_unit)
+		unit = residential_unit.unit
+		if unit
+			building = unit.building
+			if unit.building.neighborhood
+				"#{building.neighborhood.name}, #{building.sublocality}, #{building.administrative_area_level_1_short} #{building.postal_code}".freeze
+			else
+				"#{building.sublocality}, #{building.administrative_area_level_1_short} #{building.postal_code}".freeze
+			end
+		else
+			if unit.neighborhood_name
+				"#{unit.neighborhood_name}, #{unit.sublocality}, #{unit.administrative_area_level_1_short} #{unit.postal_code}".freeze
+			else
+				"#{unit.sublocality}, #{unit.administrative_area_level_1_short} #{unit.postal_code}".freeze
+			end
+		end
+
+	end
+
+	def trim_access text
+		if !text.nil? && text.size > 47
+			text[0..47] + '...'.freeze
+		else
+			text
+		end
+	end
+
+	def roommate_has_icon(roommates, idx)
+		return idx < roommates.count && roommates[idx] && roommates[idx].upload_picture_of_yourself
+	end
+
+	def beds_as_str(residential_listing)
+		if residential_listing.beds == 0
+      "Studio ".freeze
+    else
+      "#{residential_listing.beds} Beds".freeze
+    end
+	end
+
+	def baths_as_str(residential_listing)
+		if residential_listing.baths == 1
+      "#{trim_zeros(residential_listing.baths)} Bath".freeze
+    else
+      "#{trim_zeros(residential_listing.baths)} Baths".freeze
+    end
+	end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,8 @@ module RealtyHunter
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.autoload_paths += %W(#{config.root}/lib) # add this line
-    config.assets.precompile += ['application.css', 'custom.css']
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/models) # add this line
+    config.assets.precompile += ['application.css']
 
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
