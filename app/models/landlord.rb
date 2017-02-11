b4b874d74929dc029cc37d2aa2d165074acab7d6
Raquel Bujans <raquel.bujans@gmail.com>
2017-02-11 11:16:08 -0500

    exclude unit count totals from landlord audit trail

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,5 +1,5 @@
 class Landlord < ActiveRecord::Base
-  audited except: [:created_at, :updated_at]
+  audited except: [:created_at, :updated_at, :active_unit_count, :total_unit_count]
 
 	scope :unarchived, ->{where(archived: false)}
 
