5a5b7637f8adb5e884159fcbe4d759d247145d14
Raquel Bujans <raquel.bujans@gmail.com>
2017-02-20 17:37:08 -0500

    adf

--- app/serializers/listing_blob_serializer.rb
+++ app/serializers/listing_blob_serializer.rb
@@ -1,5 +1,7 @@
 # For use in our API
 class ListingBlobSerializer < ActiveModel::Serializer
   attributes :total_items, :total_pages, :page, :items
-  has_many :items, polymorphic: true #, class_name: "Listing", each_serializer: ListingSerializer
+  # has_many :items, polymorphic: true # class_name: "Listing" #, each_serializer: ListingSerializer
+  has_many :items#, polymorphic: true, each_serializer: APIResidentialListingSerializer
 end
+
