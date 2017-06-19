15b18d541fb415b7485067a84546f571eee27043
myspacenyc <rbujans@myspacenyc.com>
2017-06-19 08:35:09 -0400

    attempting to fix styles on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,10 @@
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present? #true
-  config.static_cache_control = "public, max-age=31536000"
+  # config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present? #deprecated in 5.1
+  config.public_file_server.enabled = true
+  # config.static_cache_control = "public, max-age=31536000" #deprecated in 5.1
+  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=31536000' }
   # serve from CDN
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = ENV['CLOUDFRONT_ENDPOINT']
