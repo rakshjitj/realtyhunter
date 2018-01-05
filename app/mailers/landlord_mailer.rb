70d79a14ddf5fc65cfd7bbc018aa39a9939aaace
Rakshit Joshi <rakshit@aristainfotech.com>
2018-01-05 19:30:22 +0530

    remove email at building and landlord create

--- app/mailers/landlord_mailer.rb
+++ app/mailers/landlord_mailer.rb
@@ -1,9 +1,9 @@
 class LandlordMailer < ApplicationMailer
   def send_creation_notification(landlord_id)
     @landlord = Landlord.where(id: landlord_id).first
-    mail to: ['uricohen646@gmail.com', 'rbujans@myspacenyc.com','aseinos@myspacenyc.com'],
+    mail to: ['uricohen646@gmail.com', 'rbujans@myspacenyc.com'],
       subject: "New landlord created: #{@landlord.code}",
-        reply_to: ['uricohen646@gmail.com', 'aseinos@myspacenyc.com'],
+        reply_to: ['uricohen646@gmail.com'],
         tag: 'landlord_created',
         track_opens:'true'
   end
