6d60011f8c37842c413db97efb1fe25b6f75c05e
Rakshit Joshi <rakshit@aristainfotech.com>
2018-02-06 17:06:23 +0530

    change to upload on heroku

--- db/migrate/20180205093124_add_streeteay_claim_to_residential_listings.rb
+++
@@ -1,5 +0,0 @@
-class AddStreeteayClaimToResidentialListings < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :residential_listings, :streeteasy_claim, :boolean, default: false
-  end
-end
