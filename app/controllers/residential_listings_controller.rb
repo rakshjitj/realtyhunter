898c5d718ec2a8a89be5f45c9927f8244386743e
Rakshit Joshi <rakshit@aristainfotech.com>
2018-11-13 16:37:42 +0530

    change in rental form for test

--- app/controllers/residential_listings_controller.rb
+++ app/controllers/residential_listings_controller.rb
@@ -460,6 +460,7 @@ class ResidentialListingsController < ApplicationController
 
       if params[:residential_listing][:unit][:rent]
         params[:residential_listing][:unit][:rent] = (params[:residential_listing][:unit][:gross_price].to_i * (params[:residential_listing][:lease_start].to_i - params[:residential_listing][:unit][:maths_free].to_f)) / params[:residential_listing][:lease_start].to_i
+        params[:residential_listing][:unit][:rent] = params[:residential_listing][:unit][:rent].round
       end
 
       # keep track of whether this listing just came on or off the market
