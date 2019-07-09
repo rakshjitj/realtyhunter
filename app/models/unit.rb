8e27cbbee4e1326340a52597d911b9e838d9c931
Rakshit Joshi <rakshit@aristainfotech.com>
2019-07-09 20:12:39 +0530

    add closed off status in sales listing

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -26,12 +26,12 @@ class Unit < ApplicationRecord
 	enum status: [
     :active, :pending, :off, :rsonly, #residential
     :offer_submitted, :offer_accepted, :binder_signed, :off_market_for_lease_execution, #additional for commercial
-    :on_market, :contract_out, :in_escrow, :closed # additional for sales
+    :on_market, :contract_out, :in_escrow, :closed_off, :closed # additional for sales
    ]
   validates :status, presence: true, inclusion: {
     in: ['active', 'pending', 'off', 'rsonly',
          'offer_submitted', 'offer_accepted', 'binder_signed', 'off_market_for_lease_execution',
-         'on_market', 'contract_out', 'in_escrow', 'closed'] }
+         'on_market', 'contract_out', 'in_escrow', 'closed_off', 'closed'] }
 
   enum syndication_status: ['Syndicate if matches criteria', 'Force syndicate', 'Never syndicate']
   validates :syndication_status, presence: true, inclusion: {
